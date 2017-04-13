948895c8455fa1f326a2cb5c3e8e35387dd93b2f
Kent Boogaart <kent.boogaart@gmail.com>
2017-04-13 09:59:19 +0930

    Migrate to netstandard

--- Src/Genesis.Logging.UnitTests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Genesis.Logging.UnitTests")]
-[assembly: AssemblyDescription("Contains unit tests for Genesis.Logging.")]
-[assembly: CLSCompliantAttribute(false)]
