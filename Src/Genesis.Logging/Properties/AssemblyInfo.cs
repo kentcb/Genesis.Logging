b3992cf7b42de1bd2f8a0f28ffa302c713b202e0
Kent Boogaart <kent.boogaart@gmail.com>
2017-04-13 14:21:12 +0930

    Migrate to netstandard

--- Src/Genesis.Logging/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Genesis.Logging")]
-[assembly: AssemblyDescription("Contains the implementation of Genesis.Logging.")]
-[assembly: CLSCompliantAttribute(true)]
